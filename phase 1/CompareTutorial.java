538186b747af7731b24a2114b489aef4f02d724e
Banana <thandhamma1505@gmail.com>
2025-03-30 13:53:11 +0700

    ลบ folder phrase 1 และ phrase 2 ออกจากโปรเจกต์เนื่องจาก folder ทั้งสองจัดเก็บใน Folder JavaCode เรียบร้อยแล้ว

--- phase 1/CompareTutorial.java
+++
@@ -1,8 +0,0 @@
-class CompareTutorial {
-    public static void main(String[] args) {
-        // true / false 
-            int a = 10, b=5 ;
-            boolean c=(a!=b);
-            System.out.println(c);
-    }       
-}
--- phase 1/Display.java
+++
@@ -1,8 +0,0 @@
-class Display {
-    public static void main(String[] args) {
-
-        /* Comment หลายบรรทัด */
-        System.out.print("fuck you"); //แสดงผลทางจอภาพ
-        System.out.print("Bitch");
-    }
-}
